efa217ddb1bb3d9b2c48a92220233e1d4901ab80
adam.brewer <adam.brewer@burberry.com>
2014-12-03 16:52:34 +0000

    Re-jig config, ya dig

---
+++ app/site/config/config.local.studiobrewer.com.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+
+---------------------------------------
+Kirby Configuration
+---------------------------------------
+
+By default you don't have to configure anything to
+make Kirby work. For more fine-grained configuration
+of the system, please check out http://getkirby.com/docs/advanced/options
+
+*/
+
+c::set('debug', true);
+c::set('cache', false);
