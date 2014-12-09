93a5c66ed36d8ff99f61297efedda1c72a087b4a
Adam Brewer <adamcbrewer@gmail.com>
2014-12-09 20:00:38 +0000

    Updated fonts

--- app/site/snippets/header.php
+++ app/site/snippets/header.php
@@ -2,21 +2,24 @@
 <html lang="en">
 <head>
 
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
-  <meta name="description" content="<?php echo $site->description()->html() ?>">
-  <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
+    <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+    <meta name="description" content="<?php echo $site->description()->html() ?>">
+    <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
 
-  <?php echo css('assets/css/main.css') ?>
+    <?php echo css('assets/css/main.css') ?>
+
+    <script src="//use.typekit.net/qwc7wds.js"></script>
+    <script>try{Typekit.load();}catch(e){}</script>
 
 </head>
 <body>
 
-  <header class="header cf" role="banner">
-    <a class="logo" href="<?php echo url() ?>">
-      <img src="<?php echo url('assets/images/logo.svg') ?>" alt="<?php echo $site->title()->html() ?>" />
-    </a>
-    <?php snippet('menu') ?>
-  </header>
+    <header class="header cf" role="banner">
+        <a class="logo" href="<?php echo url() ?>">
+            <img src="<?php echo url('assets/images/logo.svg') ?>" alt="<?php echo $site->title()->html() ?>" />
+        </a>
+        <?php snippet('menu') ?>
+    </header>
