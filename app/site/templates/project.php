b0fcc110f30c0404ccdd139f88c5d366f9395ef5
Adam Brewer <adamcbrewer@gmail.com>
2015-02-16 21:03:52 +0000

    Some small tweaks to the project page

--- app/site/templates/project.php
+++ app/site/templates/project.php
@@ -30,7 +30,7 @@
 
                 <article class="figure layout layout--<?php echo $image->layout() ?>">
 
-                    <figure class="project-image">
+                    <figure class="figure-image">
                         <img src="<?php echo $image->url() ?>" alt="<?php echo $image->filename() ?>">
 
                         <figcaption class="figure-caption"><?php echo $image->caption() ?></figcaption>
