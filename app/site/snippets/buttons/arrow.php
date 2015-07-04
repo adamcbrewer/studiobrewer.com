c12ea5fbc3f8f4a2d9b152dee7aa8b523cf22f41
Adam Brewer <adamcbrewer@gmail.com>
2015-07-04 08:09:17 +0100

    Tweaks form Jake

--- app/site/snippets/buttons/arrow.php
+++ app/site/snippets/buttons/arrow.php
@@ -1,4 +1,4 @@
 <svg class="svg-arrow" x="0px" y="0px" viewBox="0 0 213.2 73" enable-background="new 0 0 213.2 73" xml:space="preserve">
-    <polygon stroke-width="3" stroke-miterlimit="10" points="191.5,71.5 1.5,71.5 1.5,1.5 191.5,1.5 211.5,36.5 "/>
+    <polygon stroke-width="3" points="191.5,71.5 1.5,71.5 1.5,1.5 191.5,1.5 211.5,36.5 "/>
 </svg>
 <?php echo $copy ?>
--- app/site/snippets/svgants.php
+++ app/site/snippets/svgants.php
@@ -3,19 +3,19 @@
     <?php if ($type == 'twitter') : ?>
 
         <svg class="svg-ants svg-ants--octogon" preserveAspectRatio="xMidYMid" width="240" viewBox="0 0 277.2 277.2">
-            <path stroke-width="3" stroke-miterlimit="10" d="M231.2 37.5L132.7 1.6 37.5 46 1.6 144.5 46 239.8l98.5 35.8 95.3-44.4 35.8-98.5z"/>
+            <path stroke-width="4" stroke-miterlimit="10" d="M231.2 37.5L132.7 1.6 37.5 46 1.6 144.5 46 239.8l98.5 35.8 95.3-44.4 35.8-98.5z"/>
         </svg>
 
     <?php elseif ($type == 'pinterest') : ?>
 
         <svg class="svg-ants svg-ants--circle" preserveAspectRatio="xMidYMid" width="240" viewBox="0 0 300 300">
-            <circle cx="150" cy="150" r="148" stroke-width="4"/>
+            <circle cx="150" cy="150" r="148" stroke-width="5"/>
         </svg>
 
     <?php else : ?>
 
         <svg class="svg-ants svg-ants--square" preserveAspectRatio="xMidYMid" width="240" viewBox="0 0 300 300">
-            <polygon points="0,0 300,0 300,300 0,300" stroke-width="7" shape-rendering="crispEdges"/>
+            <polygon points="0,0 300,0 300,300 0,300" stroke-width="9" shape-rendering="crispEdges"/>
         </svg>
 
     <?php endif; ?>
