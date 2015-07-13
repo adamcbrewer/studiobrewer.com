579605df48864f1241a234c4b60d480db4973ac0
Adam Brewer <adamcbrewer@gmail.com>
2015-07-13 10:19:34 +0100

    Refactor

--- app/site/snippets/features.php
+++ app/site/snippets/features.php
@@ -3,8 +3,8 @@
     <li class="feature" title="<?php echo $project->title() ?>">
         <figure class="feature-figure figurelink">
             <a href="<?php echo $project->url() ?>">
-                <div class="rollover" data-layzr="<?php echo $project->feature()->url() ?>" data-layzr-bg>
-                    <div class="rollover-content">
+                <div class="figure-rollover" data-layzr="<?php echo $project->feature()->url() ?>" data-layzr-bg>
+                    <div class="figure-rollover-content">
                         <h3 class="feature-title"><?php echo html($project->title()) ?></h3>
                         <p class="feature-summary"><?php echo html($project->summary()) ?></p>
                     </div>
