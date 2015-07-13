05cad47cff3bd30f4b9599b1012ce5991f6f1589
Adam Brewer <adamcbrewer@gmail.com>
2015-07-13 10:30:30 +0100

    Titles under features

--- app/site/snippets/features.php
+++ app/site/snippets/features.php
@@ -3,8 +3,8 @@
     <li class="feature" title="<?php echo $project->title() ?>">
         <figure class="feature-figure figurelink">
             <a href="<?php echo $project->url() ?>">
-                <div class="figure-rollover" data-layzr="<?php echo $project->feature()->url() ?>" data-layzr-bg>
-                    <div class="figure-rollover-content">
+                <div class="rollover" data-layzr="<?php echo $project->feature()->url() ?>" data-layzr-bg>
+                    <div class="rollover-content">
                         <h3 class="feature-title"><?php echo html($project->title()) ?></h3>
                         <p class="feature-summary"><?php echo html($project->summary()) ?></p>
                     </div>
@@ -13,6 +13,10 @@
                 <noscript><img src="<?php echo $project->feature()->url() ?>" alt="<?php echo html($project->title()) ?>"></noscript>
             </a>
         </figure>
+        <footer class="feature-details">
+            <h2 class="work-title"><a href="<?php echo $project->url() ?>"><?php echo $project->title(); ?></a></h2>
+            <h3 class="work-summary"><a href="<?php echo $project->url() ?>"><?php echo $project->summary() ?></a></h3>
+        </footer>
     </li>
 
 <? endforeach; ?>
