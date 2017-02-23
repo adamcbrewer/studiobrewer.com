15f799e2ca8596e88d8d7a8a9716b63831146cfc
Adam Brewer <adamcbrewer@gmail.com>
2017-02-23 15:22:35 +0000

    Contact page bugs, Dribble ant-circle and new instagram icon

--- app/site/snippets/svgants.php
+++ app/site/snippets/svgants.php
@@ -2,19 +2,19 @@
 
     <?php if ($type == 'twitter') : ?>
 
-        <svg class="svg-ants svg-ants--octogon" preserveAspectRatio="xMidYMid" width="240" viewBox="0 0 277.2 277.2">
+        <svg class="svg-ants svg-ants--octogon animate-reverse" preserveAspectRatio="xMidYMid" width="240" viewBox="0 0 277.2 277.2">
             <path stroke-width="4" stroke-miterlimit="10" d="M231.2 37.5L132.7 1.6 37.5 46 1.6 144.5 46 239.8l98.5 35.8 95.3-44.4 35.8-98.5z"/>
         </svg>
 
-    <?php elseif ($type == 'pinterest') : ?>
+    <?php elseif ($type == 'dribble') : ?>
 
-        <svg class="svg-ants svg-ants--circle animate-clockwise" preserveAspectRatio="xMidYMid" width="240" viewBox="0 0 300 300">
+        <svg class="svg-ants svg-ants--circle" preserveAspectRatio="xMidYMid" width="240" viewBox="0 0 300 300">
             <circle cx="150" cy="150" r="148" stroke-width="5"/>
         </svg>
 
     <?php else : ?>
 
-        <svg class="svg-ants svg-ants--square animate-clockwise" preserveAspectRatio="xMidYMid" width="240" viewBox="0 0 300 300">
+        <svg class="svg-ants svg-ants--square" preserveAspectRatio="xMidYMid" width="240" viewBox="0 0 300 300">
             <polygon points="0,0 300,0 300,300 0,300" stroke-width="9" shape-rendering="crispEdges"/>
         </svg>
 
